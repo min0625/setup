7e98d92190b2e668489bf6f03b4786ae34e13c83
min0625 <70873102+min0625@users.noreply.github.com>
2025-11-14 20:17:37 +0800

    chore: Update zshrc

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,11 +1,15 @@
-# export LANG="en_US.UTF-8"
+# Zsh configuration file
+
+# Locale
 export LANG="zh_TW.UTF-8"
-# export LC_ALL="en_US.UTF-8"
+# export LANG="en_US.UTF-8"
 # export LC_ALL="zh_TW.UTF-8"
+# export LC_ALL="en_US.UTF-8"
 
-local curr_dir="$(dirname "$(realpath "${0}")")"
+# Environment
+export MIN_ZSHRC_DIR="$(dirname "$(realpath "${0}")")"
 
-alias -- -='cd -'
+# Aliases
 alias ls='ls -F --color=auto'
 alias ls='gls' # GNU ls
 alias ll='ls -l'
@@ -14,48 +18,41 @@ alias mkdir='mkdir -p'
 alias cp='cp -i -r'
 alias mv='mv -i'
 alias rm='rm -i'
-# alias rm='trash' # brew install trash
 alias python='python3'
 alias pip='pip3'
-alias py='python'
 alias k9s='LANG="en_US.UTF-8" k9s' # k9s must be in `en_US.UTF-8` locale.
 alias gls='gls -F --color=auto --group-directories-first'
 alias docker-compose='docker compose'
+alias shell='command'
 
-shell() { "$@"; }
-
-# The `time` command output format like `GNU time`
+# Time Format
 export TIMEFMT=$'\nCPU\t%P\nuser\t%*U\nsystem\t%*S\ntotal\t%*E'
 
-# Homebrew path for mac M1
+# User Local Bin
+export PATH="${PATH}:${HOME}/.local/bin"
+
+# Homebrew
 export PATH="/opt/homebrew/bin:${PATH}"
 
-export PATH="$PATH:${HOME}/.local/bin"
-
-# GNU Bin Utils
+# GNU Util
 # Install: brew install coreutils
-# Info: brew info coreutils
+# export PATH="$(brew --prefix coreutils)/libexec/gnubin:${PATH}"
 # export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:${PATH}"
 
-# Auto completion
+# Prompt
+export PROMPT='%F{184}%n%f %F{30}%~%f${vcs_info_msg_0_} %# '
+
+autoload -Uz vcs_info
+
+zstyle ':vcs_info:git:*' formats '%F{1}(%b)%f'
+
+precmd() { vcs_info; }
+
+# Auto Completion
 autoload -Uz compinit && compinit -i
 # zstyle ':completion:*' menu select
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*' list-colors ''
-
-export PROMPT='%F{184}%n%f %F{30}%~%f${vcs_info_msg_0_} %# '
-
-# Show Git info on terminal
-autoload -Uz vcs_info
-
-precmd() {
-    # Show Git info on terminal
-    vcs_info
-}
-
-# Show Git info on terminal
-# Format the vcs_info_msg_0_ variable
-zstyle ':vcs_info:git:*' formats '%F{1}(%b)%f'
 
 setopt menu_complete # auto select the first completion entry.
 setopt prompt_subst
@@ -64,7 +61,7 @@ setopt auto_menu # show completion menu on successive tab press
 setopt complete_in_word
 setopt always_to_end
 
-# Auto Suggestions
+# Auto Suggestion
 # Install: brew install zsh-autosuggestions
 source "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 # export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#ff00ff,bg=cyan,bold,underline"
@@ -77,15 +74,17 @@ export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 # export DOCKER_DEFAULT_PLATFORM=''
 
 # ASDF
-export PATH="${ASDF_DATA_DIR:-${HOME}/.asdf}/shims:${PATH}"
+export ASDF_DATA_DIR="${HOME}/.asdf"
 
-# ASDF completions
-# append completions to fpath
-fpath=("${ASDF_DATA_DIR:-${HOME}/.asdf}/completions" ${fpath})
+export PATH="${ASDF_DATA_DIR}/shims:${PATH}"
+
+# ASDF Completion
+fpath=("${ASDF_DATA_DIR}/completions" ${fpath})
 
 # ASDF DirEnv
-# source "${XDG_CONFIG_HOME:-${HOME}/.config}/asdf-direnv/zshrc"
-eval "$(direnv hook zsh)"
+if [[ -n "$(command -v direnv)" ]]; then
+    eval "$(direnv hook zsh)"
+fi
 
 # Golang
 export GOPATH="${HOME}/go"
@@ -93,10 +92,11 @@ export GOMODCACHE="${GOPATH}/pkg/mod"
 export GOBIN="${GOPATH}/bin"
 export PATH="${PATH}:${GOBIN}"
 export GOPRIVATE="github.com/min0625,gitlab.com/min0625"
-# set GOROOT
+
+# setup GOROOT
 export ASDF_GOLANG_GOPATH="${GOPATH}"
 export ASDF_GOLANG_GOBIN="${GOBIN}"
-source "${curr_dir}/set-go-env.zsh"
+source "${MIN_ZSHRC_DIR}/set-go-env.zsh"
 
 # AWS
 # Install: brew install awscli
@@ -106,6 +106,9 @@ alias -- aws.my-core-dev='AWS_PROFILE="my-core-dev" aws'
 # GCP
 # Install: brew install --cask gcloud-cli
 alias -- gcloud-auth-login='gcloud auth login'
+alias -- gcloud-auth-revoke='gcloud auth revoke'
+alias -- gcloud-auth-login-default='gcloud auth application-default login'
+alias -- gcloud-auth-revoke-default='gcloud auth application-default revoke'
 
 # Azure
 # Install: brew install azure-cli
