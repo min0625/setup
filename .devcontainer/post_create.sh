ec016b7c7a37e2a8485e74b1fd5b7781b19d6e19
Min Huang <70873102+min0625@users.noreply.github.com>
2026-05-21 07:49:17 +0000

    Update setup script: simplify Homebrew setup and add MISE globals

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Abort on errors.
+set -eo pipefail
+
+# Abort on unset variables.
+set -u
+
+if [[ -f ".devcontainer/post_create.local.sh" ]]; then
+    source ".devcontainer/post_create.local.sh"
+fi
